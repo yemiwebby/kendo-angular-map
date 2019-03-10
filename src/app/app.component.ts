5ad172f884607eb139694d9915f838843df322d8
yemiwebby <yemiwebby@gmail.com>
2019-03-10 05:36:57 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(function () {
+      $("#map").kendoMap({
+        center: [30.2681, -97.7448],
+        zoom: 3,
+        layers: [{
+          type: "tile",
+          urlTemplate: "http://a.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png",
+          attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap contributors</a>."
+        }]
+      });
+    });
+  }
 }
