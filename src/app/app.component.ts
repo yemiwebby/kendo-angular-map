9ff00e0d288ea16e7ac90d28a80dcc7a676d2e51
yemiwebby <yemiwebby@gmail.com>
2019-03-10 06:02:27 +0100

    added a map center and location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,19 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     $(document).ready(function () {
       $("#map").kendoMap({
-        center: [30.2681, -97.7448],
-        zoom: 3,
+        center: [51.505, -0.09],
+        zoom: 6,
         layers: [{
           type: "tile",
           urlTemplate: "http://a.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png",
           attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap contributors</a>."
+        }],
+        markers: [{
+          location: [52.4862, -1.8904],
+          shape: "pinTarget",
+          tooltip: {
+            content: "Austin, TX"
+          }
         }]
       });
     });
